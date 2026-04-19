4e7dbfc0849d4a33fa339a1bf7d03e8e5277b91e
Rifqy Kurniawan Fattahillah <2410631170104@student.unsika.ac.id>
2026-04-20 06:51:41 +0700

    Delete Praktikum directory

--- Praktikum/menu.php
+++
@@ -1,7 +0,0 @@
-<!-- Struktur Menu Navigasi -->
-<nav>
-    <a href="index.php?page=soal1">Soal 1 (Switch Case)</a>
-    <a href="index.php?page=soal2">Soal 2 (For Loop)</a>
-    <a href="index.php?page=soal3">Soal 3 (Foreach)</a>
-    <a href="index.php?page=soal4">Soal 4 (Ternary)</a>
-</nav>
